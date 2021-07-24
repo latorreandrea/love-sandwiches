e02a4f2c3b09ddf06950fa94e790a904f0627e51
latorreandrea <latorre.andrea.93@gmail.com>
2021-07-24 14:57:46 +0000

    collect last 5 days of sales

--- run.py
+++ run.py
@@ -76,6 +76,19 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    collect last 5 days of sales from worksheet
+    """
+    sales = SHEET.worksheet("sales")
+    
+    columns = []
+
+    for ind in range (1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    pprint(columns)
 
 
 def main():
@@ -86,4 +99,5 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches Data Automation\n")
-main()
+#main()
+get_last_5_entries_sales()
