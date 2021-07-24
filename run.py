ea08087f0cd7420ecb18555821ae0a9fc89785b5
latorreandrea <latorre.andrea.93@gmail.com>
2021-07-24 08:37:41 +0000

    add while loop that ask valid data

--- run.py
+++ run.py
@@ -17,27 +17,36 @@ def get_sales_data():
     """
     Get sales figure from the user
     """
-    print("please enter sales data from the last market")
-    print("data should be six numbers, separated by commas")
-    print("Example: 10, 20, 30, 40, 50, 60 \n")
+    while True:
+        print("please enter sales data from the last market")
+        print("data should be six numbers, separated by commas")
+        print("Example: 10, 20, 30, 40, 50, 60 \n")
 
-    data_str = input("Enter your data here:")
-    sales_data = data_str.split(",")
-    
-    validate_data(sales_data)
+        data_str = input("Enter your data here:")
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("DATA IS VALID")
+            break
+
+        
 
 def validate_data(values):
     """
-    Raise error if strings cannot be converted in integer
-    or we dont have exactly 6 values
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-        f"Exactly 6 values required, you provided {len(values)}"
-        )
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
-        print(f"Invalid data {e}, please retry.")
-
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+    
+    return True
 
 get_sales_data()
