007dd80f81e2b8bf722199ab466d3b24b6341333
latorreandrea <latorre.andrea.93@gmail.com>
2021-07-24 16:34:10 +0000

    calculate next day preparation sandwiches

--- run.py
+++ run.py
@@ -78,7 +78,7 @@ def calculate_surplus_data(sales_row):
 
 def get_last_5_entries_sales():
     """
-    collect last 5 days of sales from worksheet
+    Collect last 5 days of sales from worksheet
     """
     sales = SHEET.worksheet("sales")
     
@@ -88,7 +88,23 @@ def get_last_5_entries_sales():
         column = sales.col_values(ind)
         columns.append(column[-5:])
 
-    pprint(columns)
+    return columns
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each kind of sandwiches adding 10%
+    """
+
+    print("Calculating stock data...\n")
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 
 
 def main():
@@ -97,7 +113,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)    
     update_worksheet(new_surplus_data, "surplus")
+    sales_colummns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_colummns)
+    update_worksheet(stock_data, "stock")
+
 
 print("Welcome to Love Sandwiches Data Automation\n")
-#main()
-get_last_5_entries_sales()
+main()
